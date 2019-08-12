9612397d1fda52fdcfa45174d223c715efe81b73
user <user@DESKTOP-OH6AF6H>
2019-08-12 20:06:53 +0900

    2019.08.12 연수

--- UAI_Project/src/main/java/www/uai/com/mapper/UserSQLMapper.java
+++ UAI_Project/src/main/java/www/uai/com/mapper/UserSQLMapper.java
@@ -1,7 +1,6 @@
 package www.uai.com.mapper;
 
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
