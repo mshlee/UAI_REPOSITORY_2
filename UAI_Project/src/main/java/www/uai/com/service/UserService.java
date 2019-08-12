d6221d94eb8713417e2a1df8009f66073903e7a9
user <user@DESKTOP-OH6AF6H>
2019-08-12 20:04:32 +0900

    2019.08.12 연수

--- UAI_Project/src/main/java/www/uai/com/service/UserService.java
+++ UAI_Project/src/main/java/www/uai/com/service/UserService.java
@@ -1,5 +1,12 @@
 package www.uai.com.service;
 
-public interface UserService {
+import www.uai.com.vo.MemberDataVO;
+import www.uai.com.vo.SessionDataVO;
 
+public interface UserService {
+	public void joinMember(MemberDataVO vo);
+	
+	public SessionDataVO loginProcess(MemberDataVO vo);
+	
+	public boolean isExistID(MemberDataVO requestParam);
 }
